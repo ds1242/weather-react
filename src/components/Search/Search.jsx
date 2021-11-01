b88d6f16ddd6231fb327e99cf45f4867f751e2f4
ds1242 <david.shaw1242@gmail.com>
2021-11-01 08:09:04 -0600

    pull search value from form

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button, Container, Col, Row } from 'react-bootstrap';
 import './Search.css';
 
 
 function Search () {
+    const [cityVal, setCityVal] = useState({city: ''});
 
-    const handleFormSubmit = event => {
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setCityVal({
+            ...cityVal,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async event => {
         event.preventDefault();
-        let cityVal = document.getElementById('search-form').value
+        
         
         console.log(cityVal)
-
+        setCityVal({ city: '' });
     }
 
 
@@ -22,7 +31,7 @@ function Search () {
             <Form id='search-form' onSubmit={handleFormSubmit}>
                 <Form.Group className="mb-3">
                     <Form.Label>Enter City Name</Form.Label>
-                    <Form.Control type="input" placeholder='City Name' />
+                    <Form.Control type="text" placeholder='City Name' name='city' value={cityVal.city} onChange={handleChange} />
                 </Form.Group>
                 <Button variant='primary' type='submit'>
                     Submit
