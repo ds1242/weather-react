f47a8c0954630090a5c20e9a8fdb166942f91864
ds1242 <david.shaw1242@gmail.com>
2021-11-02 14:03:02 -0600

    updated current weather display

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,14 +6,14 @@ import { Card, ListGroup } from 'react-bootstrap';
 function WeatherCard (props) {
 
     const { date, temp, wind, uvi, weather } = props;
-    console.log(props);
+    console.log(props)
 
     return (
         <Card>
             <Card.Body>
-                <Card.Title>{date}</Card.Title>
+                <Card.Title>{props.date}</Card.Title>
                 <ListGroup variant="flush">
-                    <ListGroup.Item>Temp: {temp}</ListGroup.Item>
+                    <ListGroup.Item>Temp: {props.temp}</ListGroup.Item>
                     <ListGroup.Item>Wind: {wind}</ListGroup.Item>
                     <ListGroup.Item>UVI: {uvi}</ListGroup.Item>
                     <ListGroup.Item>Weather: {weather}</ListGroup.Item>
